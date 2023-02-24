ccc71251ad8b59623c1bf2a4accbad77ee78418a
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-02-24 13:16:11 -0300

    My Work 2 Treinamento Dynacoop

---
+++ Dynacop/Dynacoop2023.AlfaPeople.Dynamics365/Dynacoop2023.AfaPeople.MyWorkII/Controllers/ContaController.cs
@@ -0,0 +1,37 @@
+﻿using Dynacoop2023.AlfaPeople.MyWorkII.Models;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Rest;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynacoop2023.AlfaPeople.MyWorkII.Controllers
+{
+    public class ContaController
+    {
+        public IOrganizationService ServiceClient { get; set; }
+        public Conta Conta { get; set; }
+
+        public ContaController(IOrganizationService crmServiceClient)
+        {
+            ServiceClient = crmServiceClient;
+            this.Conta = new Conta(ServiceClient);
+        }
+        
+        public Entity GetAccountById(Guid id, string[] columns)
+        {
+            return Conta.GetAccountById(id, columns);
+        }     
+
+        public void IncrementOrDecrementNumberOfOpp(Entity oppAccount, bool? incrementOrDecrement)
+        {
+            Conta.IncrementOrDecrementNumberOfOpp(oppAccount, incrementOrDecrement);
+        }
+
+    }
+}
